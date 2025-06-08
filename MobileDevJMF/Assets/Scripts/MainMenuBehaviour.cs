3ec84acaa3c46cb1894be206fb7c402a44e6e42c
jmf22a <jmf22a@acu.edu>
2025-06-08 15:56:51 -0500

    Ch12-done

--- MobileDevJMF/Assets/Scripts/MainMenuBehaviour.cs
+++ MobileDevJMF/Assets/Scripts/MainMenuBehaviour.cs
@@ -27,14 +27,54 @@ public class MainMenuBehaviour : MonoBehaviour
 
     public void Start()
     {
+        /* Unpause the game if needed */
+        Time.timeScale = 1;
+
         // check for a high score and set it to our TMProUGUI
         GetAndDisplayScore();
+
+        // slide in the Panel with all the goodies
+        SlideMenuIn(controlPanel);
 
     }
 
     private void GetAndDisplayScore()
     {
         highScoreText.text = "High Score: " + PlayerPrefs.GetInt("score").ToString();
+    }
+
+   public void SlideMenuIn(GameObject obj)
+    {
+        obj.SetActive(true);
+        var rt = obj.GetComponent<RectTransform>();
+        if (rt)
+        {
+            /* Set the object's position offscreen */
+            var pos = rt.position;
+            pos.x = -Screen.width / 2;
+            rt.position = pos;
+            /* Move the object to the center of the screen
+               (x of 0 is centered) */
+            var tween = LeanTween.moveX(rt, 0, 1.5f);
+            tween.setEase(LeanTweenType.easeInOutExpo);
+            tween.setIgnoreTimeScale(true);
+        }
+    }
+
+    public void SlideMenuOut(GameObject obj)
+    {
+        var rt = obj.GetComponent<RectTransform>();
+        if (rt)
+        {
+            var tween = LeanTween.moveX(rt,
+                Screen.width / 2, 0.5f);
+            tween.setEase(LeanTweenType.easeOutQuad);
+            tween.setIgnoreTimeScale(true);
+            tween.setOnComplete(() =>
+            {
+                obj.SetActive(false);
+            });
+        }
     }
 
 }
